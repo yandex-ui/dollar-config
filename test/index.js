be8411832a42894bcbd00ba49039b91db082a489
Alexey Ten (Lynn) <alexeyten@gmail.com>
2018-05-18 15:48:57 +0300

    Allow spaces in template reference (Fixes #2)

--- test/index.js
+++ test/index.js
@@ -151,6 +151,15 @@ describe('DollarConfig', () => {
                 expect(config.get('foo', { bar: 'baz', qux: 'xyz' })).to.equal('baz/xyz');
             });
 
+            it('allow leading and trailing spaces in references', () => {
+                const config = new Config({
+                    foo: {
+                        $template: '${    bar}/${ qux }/${bar    }'
+                    }
+                });
+                expect(config.get('foo', { bar: 'baz', qux: 'xyz' })).to.equal('baz/xyz/baz');
+            });
+
             it('joins array before computing', () => {
                 const config = new Config({
                     foo: {
